9bc3c4874963a8c18dbcc287d8972ebfed78e97e
davidschober <davidschob@gmail.com>
2023-01-05 11:35:53 -0600

    updated to pytest added placeholder for iiif testing, pep8ify

--- tests/test_nuldc.py
+++ tests/test_nuldc.py
@@ -3,7 +3,7 @@ from nuldc.helpers import (get_search_results,
                            get_all_search_results,
                            get_all_iiif,
                            get_nested_field,
-                           get_work_by_id, 
+                           get_work_by_id,
                            normalize_format,
                            sort_fields_and_values
                            )
@@ -25,7 +25,7 @@ i   Pass in a 'next_url' if you want to fake a next page, otherwise pass in ''
                  "list": ["1", "2", "3"]},
                 {"id": "2",
                  "title": "2 title",
-                 'parent': {'child': 'child value2',
+                 "parent": {"child": "child value2",
                             "label": "parent1 label"},
                  "list": ["1", "2", "3"]}
             ],
@@ -53,8 +53,12 @@ def mock_dcapi_iiif():
     def _mock_dcapi_iiif(next_url):
         data = {
             "data": [
-                {"id": "1", "title": "1 title", "parent": {"child": "child value1"}},
-                {"id": "2", "title": "2 title", 'parent': {'child': 'child value2'}}],
+                {"id": "1",
+                 "title": "1 title",
+                 "parent": {"child": "child value1"}},
+                {"id": "2",
+                 "title": "2 title",
+                 "parent": {'child': 'child value2'}}],
             "pagination": {
                 "query_url": "https://fake.com",
                 "current_page": 1,
@@ -70,7 +74,12 @@ def mock_dcapi_iiif():
     return _mock_dcapi_iiif
 
 
-def test_get_all_iiif():
+def test_get_all_iiif(requests_mock,mock_dcapi_iiif):
+    #TODO
+    p1 = mock_dcapi_iiif("http://test.com/next")
+    p2 = mock_dcapi_iiif("")
+    # requests_mock.get('http://test.com/next', json=p2)
+    # result = get_all_iiif(p1,2,2) 
     pass
 
 
@@ -107,13 +116,14 @@ def test_normalize_format(mock_dcapi):
 
 def test_sort_fields_and_values(mock_dcapi):
     all_fields, all_values = sort_fields_and_values(mock_dcapi(''))
-    some_fields, some_values = sort_fields_and_values(mock_dcapi(''), fields=['id','title'])
-    
-    assert all([len(all_fields)==4, 
-                len(all_values[0])==4,
-                len(some_fields)==2, 
-                len(some_values[0])==2, 
-                "parent" in all_fields, 
+    some_fields, some_values = sort_fields_and_values(
+        mock_dcapi(''), fields=['id', 'title'])
+
+    assert all([len(all_fields) == 4,
+                len(all_values[0]) == 4,
+                len(some_fields) == 2,
+                len(some_values[0]) == 2,
+                "parent" in all_fields,
                 "parent" not in some_fields]
                )
 
@@ -126,5 +136,6 @@ def test_get_nested_field(mock_dcapi):
     child_assert = data['parent']['child']
     child = get_nested_field('parent.child', data)
     # to green this up make if,elif, else: none
-    # no_result = get_nested_field('parent.child.nothing.nothing.nothing', mock_dcapi_p1['data'][0])
+    # no_result = get_nested_field('parent.child.nothing',
+    #                            mock_dcapi_p1['data'][0])
     assert all([child == child_assert, parent == parent_assert])
