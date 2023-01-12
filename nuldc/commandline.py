1d089a74a655d8d5af1ee9025c3466136c00dc4a
davidschober <davidschob@gmail.com>
2023-01-12 17:34:06 -0600

    added model option and cleaned up cli

--- nuldc/commandline.py
+++ nuldc/commandline.py
@@ -1,19 +1,17 @@
 """NULDC
 
 USAGE:
-    nuldc works <id> [--as <format>]
-    nuldc collections <id> [--as <format>]
-    nuldc collections <id> [--as <format>] [--all]
-    nuldc search <query_string> [--as <format>] [--all]
-    nuldc search <query_string> [--csv <outfile>] [--all]
-    nuldc search <query_string> [--csv <outfile> --fields <fields>] [--all]
+    nuldc works <id> [--as=<format>]
+    nuldc collections <id> [--as=<format> --all] 
+    nuldc search <query> [--model=<model>] [--as=<format>] [--all] [--fields=<fields> --csv <file>]
 
 OPTIONS:
-    --as <format>       get results as [default: opensearch]
-    --all               get all records from search
-    --csv <outfile>     output to CSV, requied outfile
-    --fields <fields>   optional set of fields,e.g id,ark,test defaults to all
-    -h --help           Show this screen
+    --as=<format>      get results as [default: opensearch]
+    --model=<model>    search model (works,collections,filesets) [default: works]
+    --all              get all records from search
+    --csv <outfile>    output to CSV, requied outfile
+    --fields=<fields>  optional set of fields,e.g id,ark,test defaults to all
+    -h --help          Show this screen
 
 ARGUMENTS:
     as: opensearch
@@ -24,6 +22,7 @@ ARGUMENTS:
 from docopt import docopt
 from nuldc import helpers
 import json
+import sys
 
 
 def main():
@@ -38,28 +37,33 @@ def main():
         data = helpers.get_collection_by_id(api_base_url,
                                             args.get("<id>"),
                                             params,
-                                            all_results=args.get("--all"))
+                                            all_results=args.get("--all-records"))
     # search
     if args['search']:
-        params = {"query": args.get('<query_string>'),
+        
+        # Smoke test. Let's verify that they didn't try to do csv AND iiif.
+        if args["--csv"] and args["--as"]=="iiif":
+            sys.exit("Can't convert iiif to csv, sorry! Try it without the --as=iiif")
+
+        params = {"query": args.get("<query>"),
                   "as": args.get("--as"),
                   "size": "250"}
-
+        # get the data from the search results helper
         data = helpers.get_search_results(api_base_url,
+                                          args["--model"],
                                           params,
                                           all_results=args.get("--all"))
-
-        if args['--csv']:
-            if args.get('--fields'):
-                fields = args.get('--fields').split(',')
-            else:
-                fields = args.get('--fields')
-
+        # Only try to kick out csv if it's opensearch, otherwise return csv
+        if args["--csv"] and args["--as"]=="opensearch":
+            fields = args.get("--fields")
+            if fields:
+                fields = fields.split(",")
             headers, values = helpers.sort_fields_and_values(data, fields)
             helpers.save_as_csv(headers, values, args['--csv'])
             data = {"message": "saved csv to :" + args.get('--csv')}
-
-    print(json.dumps(data))
+        
+    # if there's a user message, print it otherwise dump the data
+    print(data.get("message") or json.dumps(data))
 
 
 if __name__ == '__main__':
